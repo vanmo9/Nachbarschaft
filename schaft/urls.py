598b1f9b7acf79a55d85c9fd3d14b231289481f3
mohassan <vanmowha@gmail.com>
2019-06-02 19:31:42 +0300

    imported settings in url

--- schaft/urls.py
+++ schaft/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'', views.home, name='home'),  
 
-] 
+]   
 
 if settings.DEBUG:
    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
