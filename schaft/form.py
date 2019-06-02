81f8bb8d7d1ab75a6eaf5dbd90c2950536952d55
mohassan <vanmowha@gmail.com>
2019-06-02 19:31:10 +0300

    created formspy

--- schaft/form.py
+++ schaft/form.py
@@ -13,7 +13,7 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-
+    
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
